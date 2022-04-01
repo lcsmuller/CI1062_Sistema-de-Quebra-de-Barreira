3251ac90189d235a51a0a64601df478eb7e1db9e
guipateiro <57327655+guipateiro@users.noreply.github.com>
2022-04-01 15:31:19 -0300

    Leitura simples de CSV para array

--- OpenCsvExample.java
+++ OpenCsvExample.java
@@ -1,18 +1,30 @@
 package PARADINHAS;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-
-
-
 
 public class OpenCsvExample {
 
-    
-
-        
+	private String row = null; //talves possa ser declarado na propria função
+	
+	//atualmente so pega um arquivoe imprime na tela, mas ja divide cada celula em uma variavel independente
+	public void leCsv() throws Exception {
+		//try{
+		BufferedReader csvReader = new BufferedReader(new FileReader("C:\\Users\\guipa\\Downloads\\JF_Secao_25_Ago_2020.csv")); //colocar nome do arquivo que sere passado
+		while ((row = csvReader.readLine()) != null) {
+			String[] data = row.split(";"); // mudar ; é o separador de celular do csv
+			for (int i = 0; i < data.length; i++) {
+				System.out.println(data[i] + " ");
+			}
+		}
+		csvReader.close();
+		//}catch(){}                fazer bloco catch com excessoes de IO
+	}
+	//desoceomente para testar so esse arquivo
+	/*public static void main(String args[]) throws Exception{
+		OpenCsvExample novo = new OpenCsvExample();
+		novo.leCsv();
+	}*/
     
 
 }
