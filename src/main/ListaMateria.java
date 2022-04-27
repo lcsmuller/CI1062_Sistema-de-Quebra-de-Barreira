9d5ee2d5c221ca2bbddd7fabeec93e20536a8e0d
giordanosilveira <giordanovraau0204@gmail.com>
2022-04-27 16:03:58 -0300

    Arquivos base pronto. Terminando os arquivos base, qualquer coisa podem modificar. "that's all folks!!!"

--- src/main/ListaMateria.java
+++ src/main/ListaMateria.java
@@ -1,4 +1,4 @@
-package src.main;
+package main;
 import java.util.Vector;
 
 
@@ -8,6 +8,42 @@ import java.util.Vector;
 
 public class ListaMateria {
 
-    private Vector<Materia> lista = new Vector<Materia>(); 
+    private Vector<Materia> lista = new Vector<Materia>();
 
+	public Vector<Materia> getLista() {
+		return lista;
+	}
+
+	public void setLista(Vector<Materia> lista) {
+		this.lista = lista;
+	}
+	
+	public void inserir (Materia mat) {
+		this.lista.add(mat);
+	}
+	
+	public void inserirEm(Materia mat, int pos) {
+		this.lista.add(pos, mat);
+	}
+	
+	public void remover(Materia mat) {
+		this.lista.remove(mat);
+	}
+	
+	public void removerEm (int pos) {
+		this.lista.remove(pos);
+	}
+
+	public Vector<Integer> procutarMateria (String codigoMateria) {
+		Vector<Integer>lista_indice = new Vector<>();
+		for (int i = 0; i < this.lista.size(); i++) {
+			String cod_curso_lista = this.lista.get(i).getCodigoMateria();
+			if (cod_curso_lista.equals(codigoMateria))
+				lista_indice.add(i);
+		}
+		return lista_indice;
+	}
+	
+
+		
 }
--- src/main/Materia.java
+++ src/main/Materia.java
@@ -1,4 +1,4 @@
-package src.main;
+package main;
 /** @todo gerar métodos com eclipse */
 
 public class Materia{
@@ -9,6 +9,69 @@ public class Materia{
     private String nome;
     private int periodo;
     private String tipo;
-    private int horas; 
+    private int horas;
+    
+    public Materia() {
+    }
+    
+    public Materia(String codigoCurso, int versao, String codigoMateria, String nome, int periodo, String tipo, int horas) {
+    	this.codigoCurso = codigoCurso;
+    	this.versao = versao;
+    	this.codigoMateria = codigoMateria;
+    	this.nome = nome;
+    	this.periodo = periodo;
+    	this.tipo = tipo;
+    	this.horas = horas;
+    }
+    
+	public String getCodigoCurso() {
+		return codigoCurso;
+	}
+	public void setCodigoCurso(String codigoCurso) {
+		this.codigoCurso = codigoCurso;
+	}
+	
+	public int getVersao() {
+		return versao;
+	}
+	public void setVersao(int versao) {
+		this.versao = versao;
+	}
+	
+	public String getCodigoMateria() {
+		return codigoMateria;
+	}
+	public void setCodigoMateria(String codigoMateria) {
+		this.codigoMateria = codigoMateria;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getPeriodo() {
+		return periodo;
+	}
+	public void setPeriodo(int periodo) {
+		this.periodo = periodo;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public int getHoras() {
+		return horas;
+	}
+	public void setHoras(int horas) {
+		this.horas = horas;
+	}
+
 
 }
