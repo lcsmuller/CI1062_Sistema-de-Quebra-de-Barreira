c1b7dfe91930ccf1020311ec043cd9f3db7e431d
John <joaogabrielbdm@gmail.com>
2022-04-30 17:23:03 -0300

    mexendo em interface

--- src/main/Interface.java
+++ src/main/Interface.java
@@ -1,18 +1,24 @@
 package main;
 
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.Console;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class Interface extends JFrame {
     private static Interface uniqueInstance = null;
-    private JPanel panel;
-    private JButton button;
+    private JPanel tablePanel;
+    private JPanel upperPanel;
+    private JPanel lowerPanel;
+    private JTable classTable;
     
     private Interface() {
 
@@ -28,20 +34,41 @@ public class Interface extends JFrame {
     }
 
     private void initialize() {
-        this.setTitle("Trabalho :)");
+        this.setTitle("Trabalho Paradigmas");
         this.setLayout(new BorderLayout());
-        this.setSize(600, 400);
+        this.setIconImage(null);
+        this.setSize(900, 600);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        panel = new JPanel();
-        button = new JButton();
-        panel.setLayout(new BorderLayout());
-        this.add(panel);
-        panel.add(button, BorderLayout.CENTER);
+        
+        tablePanel = new JPanel();
+        upperPanel = new JPanel();
+        lowerPanel = new JPanel();
+        classTable = new JTable();
+        
+        tablePanel.setLayout(new BorderLayout());
+        this.add(tablePanel, BorderLayout.CENTER);
+        
+        upperPanel.setLayout(new FlowLayout());
+        this.add(upperPanel, BorderLayout.NORTH);
+        
+        lowerPanel.setLayout(new FlowLayout());
+        this.add(lowerPanel, BorderLayout.SOUTH);
+        
+        JScrollPane scrollPane = new JScrollPane(classTable);
+        tablePanel.add(scrollPane);
+        
+        upperPanel.add(new JLabel("among us"));
+        upperPanel.add(new JLabel("sus"));
+        upperPanel.add(new JButton("pipis"));
+        
+        lowerPanel.add(new JLabel("sonegação de imposto"));
     }
-/* 
+ 
     public static void main(String args[]) {
         Interface frame = getInstance();
+        
     }
-*/
+
 }
