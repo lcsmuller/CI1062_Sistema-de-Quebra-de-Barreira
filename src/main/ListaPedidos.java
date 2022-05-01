3e23d3a3eed971b42e4ab4c6858d76924d4e10a8
guipateiro <57327655+guipateiro@users.noreply.github.com>
2022-04-30 23:37:17 -0300

    Add files via upload

--- src/main/ListaPedidos.java
+++ src/main/ListaPedidos.java
@@ -1,37 +1,34 @@
 package main;
 
-import java.util.Arrays;
-
+/** Lista de matérias que podem ser pedidas pelo aluno (não cursadas). */
 public class ListaPedidos extends Tokenizador<Materia> {
-	
-	public AlunoMateria fromLinhaTokens(String[] entrada){
+
+	/**
+	 * Transforma tokens de linha CSV obtidos por
+	 *		{@link main.Csv#tokeniza(String)}.
+	 *
+	 * @param tokensLinha os tokens a serem serializados
+	 * @return resultado da transformação em AlunoMateria
+	 */
+	public AlunoMateria fromLinhaTokens(String[] tokensLinha){
 		AlunoMateria nova = new AlunoMateria();
 
-  		nova.setCodigoCurso(entrada[0]);
-  		nova.setVersao(Integer.parseInt(entrada[1]));
-  		nova.setCodigoMateria(entrada[2]);
-  		nova.setNome(entrada[3]);
+  		nova.setCodigoCurso(tokensLinha[0]);
+  		nova.setVersao(Integer.parseInt(tokensLinha[1]));
+  		nova.setCodigoMateria(tokensLinha[2]);
+  		nova.setNome(tokensLinha[3]);
 		nova.setPeriodo(0);
-  		nova.setTipo(entrada[4]);
-  		nova.setHoras(Integer.parseInt(entrada[5]));
-  		nova.setNota(Integer.parseInt(entrada[6]));
+  		nova.setTipo(tokensLinha[4]);
+  		nova.setHoras(Integer.parseInt(tokensLinha[5]));
+  		nova.setNota(Integer.parseInt(tokensLinha[6]));
   		try {
-  			nova.setFrequencia(Integer.parseInt(entrada[7]));
-  		}catch(Exception erro){
+  			nova.setFrequencia(Integer.parseInt(tokensLinha[7]));
+  		} catch(Exception e) {
   			nova.setFrequencia(0);
   		}
-  		nova.setSituacao(entrada[15]);
+  		nova.setSituacao(tokensLinha[15]);
 
   		return nova;
-	}
-	
-	public String[][] toTokens(){
-		String[][] data = new String[2000][1];
-		int i;
-		for(i = 0; i < this.tamanhoLista(); ++i) {									
-			data[i] = this.listaGetAt(i).imprimivel().split(",");	//get at retorno o elemento na posicao i , imprimivel torna o objeto Materia imprimivel com o println 
-		}
-		return Arrays.copyOf(data, i);
 	}
 
 }
